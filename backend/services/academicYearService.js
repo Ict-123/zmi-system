b78395623cd1134e64f592c1f6f706a86b031b7e
Ict-123 <107621172+Ict-123@users.noreply.github.com>
2026-04-14 16:48:53 +0000

    Create academicYearService.js with functions for managing academic years and students

---
+++ backend/services/academicYearService.js
@@ -0,0 +1,36 @@
+// academicYearService.js
+
+/**
+ * Starts a new academic year.
+ */
+function startNewAcademicYear(year) {
+    // Logic to start a new academic year
+}
+
+/**
+ * Archives the previous academic year.
+ */
+function archivePreviousYear(year) {
+    // Logic to archive previous academic year
+}
+
+/**
+ * Promotes students to the next grade level.
+ */
+function promoteStudents(students) {
+    // Logic to promote students
+}
+
+/**
+ * Moves graduates to alumni status.
+ */
+function moveGraduatesToAlumni(graduates) {
+    // Logic to move graduates to alumni
+}
+
+module.exports = {
+    startNewAcademicYear,
+    archivePreviousYear,
+    promoteStudents,
+    moveGraduatesToAlumni,
+};
